9e7ff461bd724dbe62f63dad647f41c9a1773a10
GitAarontell <70613159+GitAarontell@users.noreply.github.com>
2022-05-14 13:30:10 -0700

    Update tests.py

--- tests.py
+++ tests.py
@@ -11,7 +11,9 @@ class TestCase(unittest.TestCase):
     def test2(self):
         expected = "Goodbye World"
         self.assertNotEqual(task.my_func(), expected)
-
+    def test3(self):
+        expected = "Goodbye"
+        self.assertNotEqual(task.my_func(), expected)
 
 if __name__ == '__main__':
     unittest.main()
